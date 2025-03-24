66e31a824ace4d6df3cabd33286e73be32d377bf
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-03-24 20:59:37 +0530

    added auth and root directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,10 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <body
-        className={`${monaSans.className}  antialiased`}
-      >
+    <html lang="en" className="dark" suppressHydrationWarning>
+      <body className={`${monaSans.className} antialiased`}>
         {children}
       </body>
     </html>
