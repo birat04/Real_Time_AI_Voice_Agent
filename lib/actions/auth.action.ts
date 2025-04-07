220e3728b39b7451b5f54565edcf966c28ab1442
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-04-08 00:51:13 +0530

    add signup in actions

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -22,10 +22,42 @@ export async function setSessionCookie(idToken: string) {
   });
 }
 
-export async function signUp(params: SignUpParams){
-  const {uid, name, email} = params;
+export async function signUp(params: SignUpParams) {
+  const { uid, name, email } = params;
+
+  try {
+    const userRecord = await db.collection("users").doc(uid).get();
+    if (userRecord.exists)
+      return {
+        success: false,
+        message: "User already exists. Please sign in.",
+      };
+    await db.collection("users").doc(uid).set({
+      name,
+      email
+    });
+    return {
+      success: true,
+      message: "Account created successfully. Please sign in.",
+    };
+  } catch (error: any) {
+    console.error("Error creating user:", error);
+    if (error.code === "auth/email-already-exists") {
+      return {
+        success: false,
+        message: "This email is already in use",
+      };
+    }
+    return{
+      success: false,
+      message: "An error occurred while creating the account.",
+    }
+  }
+
+    
 }
 export async function signIn(params: SignUpParams){
+
 }
 export async function signOut(params: SignUpParams){
   const {uid, name, email} = params;
