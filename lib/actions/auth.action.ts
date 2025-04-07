fef85744c8d54657e750591655444742fceada16
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-04-07 21:24:32 +0530

    auth.action in lib

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,3 +1,32 @@
-export async function isAuthenticated() {
-    return ;
-  }
+"use server";
+
+import {auth, db} from "@/firebase/admin";
+import { Cookies } from "next/headers";
+import path from "path";
+
+const SESSION_DURATION = 60 * 60 * 24 * 7; 
+
+export async function setSessionCookie(idToken: string) {
+  const cookies = Cookies();
+
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: SESSION_DURATION * 1000,
+  });
+
+  cookies.set("session", sessionCookie, {
+    maxAge: SESSION_DURATION,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
+}
+
+export async function signUp(params: SignUpParams){
+  const {uid, name, email} = params;
+}
+export async function signIn(params: SignUpParams){
+}
+export async function signOut(params: SignUpParams){
+  const {uid, name, email} = params;
+}
